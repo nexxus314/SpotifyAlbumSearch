b0c6b5a70cd633c5953270b8e7bbb952a143c1d0
Adhithyan <nexxusromsbeta@gmail.com>
2025-05-17 12:22:11 +0530

    made changes to css, added some animations

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  
   plugins: [react()],
+
+   server: {
+    allowedHosts: ['8699-2409-40f2-3f-3a11-bba9-5fe2-d318-f8ba.ngrok-free.app'],
+   }
 })
